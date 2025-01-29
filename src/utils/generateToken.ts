3f1c486a125837fe08f81ae4e2073793a45c5cfc
ilyess.torki.v <ilyess.torki.v@gmail.com>
2025-01-29 15:03:37 +0000

    update the project

--- src/utils/generateToken.ts
+++ src/utils/generateToken.ts
@@ -2,8 +2,12 @@ import jwt from "jsonwebtoken";
 import { Types } from "mongoose";
 
 const generateToken = (userId: Types.ObjectId) =>
-  jwt.sign({ userId }, process.env.JWT_SECRET_KEY, {
-    expiresIn: process.env.JWT_EXPIRES_IN,
-  });
+  jwt.sign(
+    { exp: process.env.JWT_EXPIRES_IN, data: userId },
+    process.env.JWT_SECRET_KEY
+  );
 
+// { userId }, process.env.JWT_SECRET_KEY, {
+//   expiresIn: process.env.JWT_EXPIRES_IN,
+// }
 export default generateToken;
