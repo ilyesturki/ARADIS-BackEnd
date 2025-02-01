c01a6e82edfeb8ad2bb390799703572f82cc06aa
ilyess.torki.v <ilyess.torki.v@gmail.com>
2025-02-01 23:06:08 +0000

    update the project

--- src/utils/generateToken.ts
+++ src/utils/generateToken.ts
@@ -1,7 +1,7 @@
 import jwt from "jsonwebtoken";
 
-const generateToken = (payload: string) =>
-  jwt.sign({ payload }, process.env.JWT_SECRET_KEY as string, {
+const generateToken = (id: string) =>
+  jwt.sign({ id }, process.env.JWT_SECRET_KEY as string, {
     expiresIn: process.env.JWT_EXPIRES_IN as any,
   });
 // jwt.sign(
