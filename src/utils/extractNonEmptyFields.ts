d110b7bf0423bdb26db66610e42e8e8cf0cb6675
ilyess.torki.v <ilyess.torki.v@gmail.com>
2025-02-01 13:31:16 +0000

    update the project

--- src/utils/extractNonEmptyFields.ts
+++ src/utils/extractNonEmptyFields.ts
@@ -1,17 +1,20 @@
-const extractNonEmptyFields = <T>(requestBody: any, model: any): Partial<T> => {
+const extractNonEmptyFields = <T extends Record<string, any>>(
+  requestBody: any,
+  model: any
+): Partial<T> => {
   const notEmptyData: Partial<T> = {};
 
   for (const key of Object.keys(model.schema.paths)) {
     if (requestBody[key]) {
-      notEmptyData[key] = requestBody[key];
+      notEmptyData[key as keyof T] = requestBody[key]; // Type assertion here
     }
   }
+
   console.log("//");
   console.log(Object.keys(model.schema.paths));
   console.log(requestBody);
   console.log(notEmptyData);
   console.log("//");
+
   return notEmptyData;
 };
-
-export default extractNonEmptyFields;
